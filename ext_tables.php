d8d54b6b0b1c8cd1c3a331f7b22b1201ccf78ec2
Helmut Hummel <helmut.hummel@typo3.org>
2015-08-15 19:17:02 +0200

    Whitespace fix

--- ext_tables.php
+++ ext_tables.php
@@ -1,18 +1,18 @@
 <?php
-	defined ('TYPO3_MODE') or die('Access denied.');
+defined ('TYPO3_MODE') or die('Access denied.');
 
-	if (TYPO3_MODE == 'BE') {
-		t3lib_extMgm::addModule('tools', 'txnawsinglesignonM1', '', t3lib_extMgm::extPath($_EXTKEY) . 'mod1/');
-		$GLOBALS['TBE_MODULES_EXT']['xMOD_db_new_content_el']['addElClasses']['tx_nawsinglesignon_pi1_wizicon'] = t3lib_extMgm::extPath($_EXTKEY) . 'Classes/Plugin/class.tx_nawsinglesignon_pi1_wizicon.php';
-	}
+if (TYPO3_MODE == 'BE') {
+	t3lib_extMgm::addModule('tools', 'txnawsinglesignonM1', '', t3lib_extMgm::extPath($_EXTKEY) . 'mod1/');
+	$GLOBALS['TBE_MODULES_EXT']['xMOD_db_new_content_el']['addElClasses']['tx_nawsinglesignon_pi1_wizicon'] = t3lib_extMgm::extPath($_EXTKEY) . 'Classes/Plugin/class.tx_nawsinglesignon_pi1_wizicon.php';
+}
 
-	if (is_callable('t3lib_div::loadTCA')) {
-		t3lib_div::loadTCA('tt_content');
-	}
+if (is_callable('t3lib_div::loadTCA')) {
+	t3lib_div::loadTCA('tt_content');
+}
 
-	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1'] = 'layout,select_key,pages';
-	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1'] = 'layout,select_key,pages';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
 
-	t3lib_extMgm::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:naw_single_signon/flexform_ds.xml');
-	t3lib_extMgm::addPlugin(array('LLL:EXT:naw_single_signon/Resources/Private/Language/locallang_tca.php:naw_single_signon', 'naw_single_signon_pi1'));
+t3lib_extMgm::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:naw_single_signon/flexform_ds.xml');
+t3lib_extMgm::addPlugin(array('LLL:EXT:naw_single_signon/Resources/Private/Language/locallang_tca.php:naw_single_signon', 'naw_single_signon_pi1'));
 

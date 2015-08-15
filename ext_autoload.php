efa71709ee41c7a0aef126e9625f0343f9e8460c
Helmut Hummel <helmut.hummel@typo3.org>
2015-08-15 13:04:43 +0200

    Add autoload registry for all classes

---
+++ ext_autoload.php
@@ -0,0 +1,9 @@
+<?php
+$extPath = t3lib_extMgm::extPath('naw_single_signon');
+
+return array(
+	'tx_nawsinglesignon_pi1' => $extPath . 'pi1/class.tx_nawsinglesignon_pi1.php',
+	'tx_nawsinglesignon_pi1_wizicon' => $extPath . 'pi1/class.tx_nawsinglesignon_pi1_wizicon.php',
+	'tx_nawsinglesignon_usermapping' => $extPath . 'class.tx_nawsinglesignon_usermapping.php',
+	'tx_nawsinglesignon_module1' => $extPath . 'mod1/index.php',
+);
--- ext_tables.php
+++ ext_tables.php
@@ -10,11 +10,9 @@
 		t3lib_div::loadTCA('tt_content');
 	}
 
-	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1'] = 'layout,select_key,pages';
-	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1'] = 'pi_flexform';
+	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1'] = 'layout,select_key,pages';
+	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
 
-	t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:naw_single_signon/flexform_ds.xml');
-	t3lib_extMgm::addPlugin(Array('LLL:EXT:naw_single_signon/locallang_tca.php:naw_single_signon', 'naw_single_signon_pi1'));
-
-	include_once t3lib_extMgm::extPath('naw_single_signon') . 'class.tx_nawsinglesignon_usermapping.php';
+	t3lib_extMgm::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:naw_single_signon/flexform_ds.xml');
+	t3lib_extMgm::addPlugin(array('LLL:EXT:naw_single_signon/locallang_tca.php:naw_single_signon', 'naw_single_signon_pi1'));
 

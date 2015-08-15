0d89422fb1df5868fa4e481d36139ce207887f65
Helmut Hummel <helmut.hummel@typo3.org>
2015-08-15 13:22:17 +0200

    Raise PHP version constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
-			'php' => '5.2.0-5.3.99',
+			'php' => '5.2.0-5.6.99',
 			'typo3' => '4.5.0-4.7.99',
 		),
 		'conflicts' => array(

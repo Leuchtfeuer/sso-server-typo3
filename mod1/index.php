1bb4588f9ca9b15114acd20da22d3b9c66c24519
Helmut Hummel <helmut.hummel@typo3.org>
2015-11-04 13:00:18 +0100

    Use global $LANG variable

--- mod1/index.php
+++ mod1/index.php
@@ -1,5 +1,5 @@
 <?php
-$LANG->includeLLFile('EXT:single_signon/mod1/locallang.xml');
+$GLOBALS['LANG']->includeLLFile('EXT:single_signon/mod1/locallang.xml');
 // This checks permissions and exits if the users has no permission for entry.
 $BE_USER->modAccess($MCONF, 1);
 

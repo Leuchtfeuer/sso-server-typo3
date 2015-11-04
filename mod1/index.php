35d79fd3133cefbeff1e1b3c5d0114b7229f2893
Helmut Hummel <helmut.hummel@typo3.org>
2015-11-04 13:00:32 +0100

    Use global $BE_USER variable

--- mod1/index.php
+++ mod1/index.php
@@ -1,7 +1,7 @@
 <?php
 $GLOBALS['LANG']->includeLLFile('EXT:single_signon/mod1/locallang.xml');
 // This checks permissions and exits if the users has no permission for entry.
-$BE_USER->modAccess($MCONF, 1);
+$GLOBALS['BE_USER']->modAccess($MCONF, 1);
 
 /** @var tx_singlesignon_module1 $SOBE */
 $SOBE = t3lib_div::makeInstance('tx_singlesignon_module1');

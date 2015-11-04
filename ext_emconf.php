d7e788bd45026b59419189a9e3440402d428f926
Helmut Hummel <helmut.hummel@typo3.org>
2015-11-04 12:46:27 +0100

    Raise TYPO3 version to 6.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.2.0-5.6.99',
-			'typo3' => '4.5.39-4.7.99',
+			'typo3' => '6.2.0-7.99.99',
 		),
 		'conflicts' => array(
 		),

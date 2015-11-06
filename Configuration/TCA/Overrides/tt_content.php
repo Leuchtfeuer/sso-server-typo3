143e69a906abd8c0a5800ab250e7c2294f09e2d7
Helmut Hummel <helmut.hummel@typo3.org>
2015-11-06 12:49:43 +0100

    Move TCA configuration to overrides to have them cached

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,23 @@
+<?php
+namespace Bitmotion\SingleSignon\Configuration\TCA\Overrides;
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+$extensionKey = 'single_signon';
+ExtensionManagementUtility::addPlugin(
+	array('LLL:EXT:single_signon/Resources/Private/Language/locallang_tca.php:single_signon', 'single_signon_pi1'),
+	'list_type', 
+	$extensionKey
+);
+ExtensionManagementUtility::addStaticFile(
+	$extensionKey, 
+	'pi1/static/', 
+	'Bitmotion SSO'
+);
+ExtensionManagementUtility::addPiFlexFormValue(
+	$extensionKey . '_pi1', 
+	'FILE:EXT:single_signon/Configuration/Flexform/flexform_ds.xml'
+);
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$extensionKey . '_pi1'] = 'layout,select_key,pages';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$extensionKey . '_pi1'] = 'pi_flexform';
+unset($extensionKey);
--- ext_tables.php
+++ ext_tables.php
@@ -5,12 +5,3 @@ if (TYPO3_MODE == 'BE') {
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule('tools', 'txsinglesignonM1', '', \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'mod1/');
 	$GLOBALS['TBE_MODULES_EXT']['xMOD_db_new_content_el']['addElClasses']['Bitmotion\\SingleSignon\\Plugin\\Wizicon'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Plugin/Wizicon.php';
 }
-
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1'] = 'layout,select_key,pages';
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:single_signon/Configuration/Flexform/flexform_ds.xml');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(array('LLL:EXT:single_signon/Resources/Private/Language/locallang_tca.php:single_signon', 'single_signon_pi1'));
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'pi1/static/', 'Bitmotion SSO');
-

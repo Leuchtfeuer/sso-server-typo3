5adef97e1bb8949563b575db3e7ee8877fa8e9c5
Helmut Hummel <helmut.hummel@typo3.org>
2015-11-06 11:56:29 +0100

    Fix UserDataSourceInterface check

--- Migrations/Code/ClassAliasMap.php
+++ Migrations/Code/ClassAliasMap.php
@@ -2,5 +2,4 @@
 
 return array(
 	'tx_singlesignon_pi1' => 'Bitmotion\\SingleSignon\\Plugin\\PluginController',
-	'Tx_SingleSignon_UserData_FrontendUserDataSource' => 'Bitmotion\\SingleSignon\\UserData\\FrontendUserDataSource',
 );

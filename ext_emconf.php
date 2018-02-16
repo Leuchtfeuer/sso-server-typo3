b73203e7d14380e14fa29a01aec7b7502aa2d4d3
Florian Wessels <f.wessels@bitmotion.de>
2018-02-16 09:33:42 +0100

    [RELEASE] Release of version 3.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
     'description' => 'The TYPO3 SSO Server provides seamless integration of third-party (i.e. non-TYPO3) applications (SSO Apps) into TYPO3. This includes end-user access to SSO Apps with no additional logon.',
     'category' => 'plugin',
     'shy' => 0,
-    'version' => '3.0.0',
+    'version' => '3.0.1',
     'conflicts' => '',
     'priority' => '',
     'loadOrder' => '',
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
     'CGLcompliance_note' => '',
     'constraints' => array(
         'depends' => array(
-            'php' => '5.2.0-5.6.99',
+            'php' => '5.2.0-7.0.99',
             'typo3' => '6.2.0-7.99.99',
         ),
         'conflicts' => array(
